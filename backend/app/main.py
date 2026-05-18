fc2b3b9efaa0e8f26634215efa3b058a1a3928b6
Shivam Khokhani <sxk2125@mavs.uta.edu>
2026-05-18 17:43:05 -0500

    Resolved CORS issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,15 +3,32 @@ from fastapi import FastAPI
 from app.database.connection import engine, Base
 from app.routes import proxy, events, analytics
 from app.models.llm_event import LLMEvent
+from fastapi.middleware.cors import CORSMiddleware
 
 
 
-Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI(
     title="CostLens API",
     version="0.1.0"
 )
+
+app.add_middleware(
+    CORSMiddleware,
+
+    allow_origins = [
+        "http://localhost:3000"
+    ],
+
+    allow_credentials = True,
+
+    allow_methods = ["*"],
+
+    allow_headers = ["*"],
+)
+
+Base.metadata.create_all(bind=engine)
 
 @app.get('/')
 def root():
